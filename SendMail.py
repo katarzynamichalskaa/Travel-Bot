04b04c8c3a49155e8fab39d03e095865d7d8335c
Katarzyna Michalska <kate.michalska7@gmail.com>
2023-07-09 18:02:55 +0200

    FinalProject

--- SendMail.py
+++ SendMail.py
@@ -6,9 +6,10 @@ from email.mime.multipart import MIMEMultipart
 def send_mail():
 
     my_message = BudgetFlight('Anywhere', 0, 0)
-    sender_email = " "
-    sender_password = " "
-    receiver_email = " "
+    sender_email = "poetrybot02@gmail.com"
+    sender_password = "cutjqfpoaphszbdz"
+    #receiver_email = "michalskit42@interia.pl"
+    receiver_email = "kate.michalska7@gmail.com"
 
     subject = "Tanie loty"
     body = str(my_message)
