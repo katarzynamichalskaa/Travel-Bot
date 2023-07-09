cd94dcf1b53ff7dd3074ae34f7bcd2cee3f2423c
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-07-09 12:32:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-from BudgetFlight import BudgetFlight
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from SendMail import send_mail
+import time
+import schedule
 
-my_message = BudgetFlight('Anywhere', 0,0)
+schedule.every().sunday.at("11:40").do(send_mail)
 
-sender_email = " "
-sender_password = " "
-receiver_email = " "
-
-subject = "Budget flights"
-body = str(my_message)
-message = MIMEMultipart()
-message["From"] = " "
-message["To"] = receiver_email
-message["Subject"] = subject
-message.attach(MIMEText(body, "plain"))
-
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(sender_email, sender_password)
-
-server.sendmail(sender_email, receiver_email, message.as_string())
-
-server.quit()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
