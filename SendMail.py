723410ca06ea4a55fb7290f1350c5a5f4268ae6b
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-07-09 18:05:47 +0200

    Update SendMail.py

--- SendMail.py
+++ SendMail.py
@@ -6,12 +6,11 @@ from email.mime.multipart import MIMEMultipart
 def send_mail():
 
     my_message = BudgetFlight('Anywhere', 0, 0)
-    sender_email = "poetrybot02@gmail.com"
-    sender_password = "cutjqfpoaphszbdz"
-    #receiver_email = "michalskit42@interia.pl"
-    receiver_email = "kate.michalska7@gmail.com"
+    sender_email = " "
+    sender_password = " "
+    receiver_email = " "
 
-    subject = "Tanie loty"
+    subject = "Cheap Flights"
     body = str(my_message)
     message = MIMEMultipart()
     message["From"] = 'Travel Bot'
